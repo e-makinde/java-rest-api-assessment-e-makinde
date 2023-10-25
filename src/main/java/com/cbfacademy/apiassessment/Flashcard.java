04ee2a4068519b08497471d2ac845cbc68038a26
Esther Makinde <esthermakinde@hotmail.com>
2023-10-25 17:20:16 +0100

    Created flashcard object

---
+++ src/main/java/com/cbfacademy/apiassessment/Flashcard.java
@@ -0,0 +1,57 @@
+package com.cbfacademy.apiassessment;
+
+import java.util.UUID;
+
+public class Flashcard {
+    UUID id;
+    String flashcardQuestion;
+    String flashcardAnswer;
+    String difficulty;
+    String topic;
+
+    public Flashcard(String flashcardQuestion, String flashcardAnswer, String difficulty, String topic) {
+        this.id = UUID.randomUUID();
+        this.flashcardQuestion = flashcardQuestion;
+        this.flashcardAnswer = flashcardAnswer;
+        this.difficulty = difficulty;
+        this.topic = topic;
+    }
+
+    //GETTERS
+    public UUID getID() {
+        return this.id;
+    }
+
+    public String getFlashcardQuestion() {
+        return this.flashcardQuestion;
+    }
+
+    public String getFlashcardAnswer() {
+        return this.flashcardAnswer;
+    }
+
+    public String getDifficulty() {
+        return this.difficulty;
+    }
+
+    public String getTopic() {
+        return this.topic;
+    }
+
+    //SETTERS
+    public void setFlashcardQuestion(String flashcardQuestion) {
+        this.flashcardQuestion = flashcardQuestion;
+    }
+
+    public void setFlashcardAnswer(String flashcardAnswer) {
+        this.flashcardAnswer = flashcardAnswer;
+    }
+    
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }    
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+}
