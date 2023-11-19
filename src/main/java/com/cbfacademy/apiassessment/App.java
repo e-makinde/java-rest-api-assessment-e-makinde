b49e60660167f6fedc43b19e769ef80b9c1597f5
Esther Makinde <esthermakinde@hotmail.com>
2023-11-19 15:00:54 +0000

    Added swagger dependency, swagger annotations and swagger comments for documentation. Also removed Hello World/greeting end-point & test

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -2,9 +2,8 @@ package com.cbfacademy.apiassessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
@@ -13,8 +12,4 @@ public class App {
 		SpringApplication.run(App.class, args);
 	}
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s", name);
-	}
 }
--- src/main/java/com/cbfacademy/apiassessment/controller/FlashcardController.java
+++ src/main/java/com/cbfacademy/apiassessment/controller/FlashcardController.java
@@ -6,6 +6,11 @@ import java.util.UUID;
 import com.cbfacademy.apiassessment.response.ResponseHandler;
 import com.cbfacademy.apiassessment.service.flashcardServiceImp;
 import com.cbfacademy.apiassessment.model.Flashcard;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,32 +20,68 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/flashcard")
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact (name = "Esther", email = "esthermakinde@hotmail.com"),
+                title = "Flashcard API Assessment",
+                description = "This is a Flashcard revision service that allows users to store their desired flashcards and generate quizzes."
+        ),
+        servers = @Server(
+                description = "local environment",
+                url = "http://localhost:8080")
+)
 public class FlashcardController {
+
+
 
     @Autowired
     private flashcardServiceImp flashcardServiceImp;
 
-
-    //Return all questions and answers that have been saved
+    @Operation(
+            description = "Reads the JSON file of flashcards into an array list of Flashcard objects. Retrieves the Flashcard array.",
+            summary = "Gets all stored flashcards."
+    )
 	@GetMapping("/all")
     public ResponseEntity<Object> getAllFlashcards() {
         return ResponseHandler.responseBuilder("Here are all of the stored flashcards", HttpStatus.OK, flashcardServiceImp.getAllFlashcards());
     }
 
+
+
+    @Operation(
+            description = "Reads the JSON file of flashcards into an array list of Flashcard objects. Uses a linear sort algorithm to retrieve the specific Flashcard with the given ID in the array list.",
+            summary = "Gets the flashcard with the specified ID."
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Object> getFlashcardByID(@PathVariable("id") UUID id) {
         return ResponseHandler.responseBuilder("Here is the requested flashcard", HttpStatus.OK, flashcardServiceImp.getFlashcard(id));
     }
 
-    //Return an individual question using the ID
+
+
+    @Operation(
+            description = "Reads the JSON file of flashcards into an array list of Flashcard objects. Uses a linear search algorithm to find the specific Flashcard with the given ID in the array list. Retrieves the question attribute of the found flashcard",
+            summary = "Gets the Question of the flashcard with the specified ID."
+    )
     @GetMapping("/question/{id}")
     public ResponseEntity<Object> getQuestionByID(@PathVariable("id") UUID id) { return ResponseHandler.responseBuilder("Here is the requested question", HttpStatus.OK, flashcardServiceImp.getQuestion(id));}
 
-    //Return an individual answer using the ID
+
+
+    @Operation(
+            description = "Reads the JSON file of flashcards into an array list of Flashcard objects. Uses a linear search algorithm to find the specific Flashcard with the given ID in the array list. Retrieves the answer attribute of the found flashcard",
+            summary = "Gets the Answer of the flashcard with the specified ID."
+    )
     @GetMapping("/answer/{id}")
     public ResponseEntity<Object> getAnswerByID(@PathVariable("id") UUID id) { return ResponseHandler.responseBuilder("Here is the requested answer", HttpStatus.OK, flashcardServiceImp.getAnswer(id));}
 
-    //Get all questions with a certain difficulty
+
+
+    @Operation(
+            description = "Reads the JSON file of flashcards into an array list of Flashcard objects. Uses a linear sort algorithm to find the Flashcard objects with the given difficulty. Each Flashcard with the given difficulty is appended to a new array list. API returns this list.",
+            summary = "Gets all Flashcards with the specified difficulty in URI."
+    )
     @GetMapping("/questions/difficulty/{difficulty}")
     public ResponseEntity<Object> getQuestionsByDifficulty(@PathVariable("difficulty") String difficulty) {
         ArrayList<Flashcard> flashcards = flashcardServiceImp.getAllFlashcards();
@@ -54,7 +95,12 @@ public class FlashcardController {
         return ResponseHandler.responseBuilder("Here are flashcards with the same difficulty.", HttpStatus.OK, sameDifficultyFlashcards);
     }
 
-    //Get all questions within a certain topic
+
+
+    @Operation(
+            description = "Reads the JSON file of flashcards into an array list of Flashcard objects. Uses a linear sort algorithm to find the Flashcard objects with the given topic. Each Flashcard with the given topic is appended to a new array list. API returns this list.",
+            summary = "Gets all Flashcards with the specified topic in URI."
+    )
     @GetMapping("/questions/topic/{topic}")
     public ResponseEntity<Object> getQuestionsByTopic(@PathVariable("topic") String topic) {
         ArrayList<Flashcard> flashcards = flashcardServiceImp.getAllFlashcards();
@@ -69,6 +115,11 @@ public class FlashcardController {
     }
 
 
+
+    @Operation(
+            description = "Reads the JSON file of stored flashcards into an array list of Flashcard objects. The new Flashcard object is added the arraylist and the JSON file is overwritten with the updated array list of Flashcard objects.",
+            summary = "Adds a new Flashcard"
+    )
     @PostMapping(path="/new", produces="application/json")
     public String createFlashcard(@Valid @RequestBody Flashcard flashcard) {
         flashcardServiceImp.addFlashcard(flashcard);
@@ -76,6 +127,11 @@ public class FlashcardController {
     }
 
 
+
+    @Operation(
+            description = "Reads the JSON file of stored flashcards into an array list of Flashcard objects. Iterates through this list to find a Flashcard object with the same ID as the flashcard provided by the client. Updates all attributes of the stored flashcard with the values provided by the client and then overwrite the JSON file with the updated array list.",
+            summary = "Updates a stored flashcard"
+    )
     @PutMapping(path = "/update", produces = "application/json")
     public String updateFlashcard(@Valid @RequestBody Flashcard flashcard) {
         flashcardServiceImp.updateFlashcard(flashcard);
@@ -83,6 +139,11 @@ public class FlashcardController {
     }
 
 
+
+    @Operation(
+            description = "Reads the JSON file of stored flashcards into an array list of Flashcard objects. Iterates through this list to find a Flashcard object with the same ID as provided. Removes this flashcard from the array list and then overwrites JSON file with the updated array list.",
+            summary = "Deletes a stored flashcard"
+    )
     @DeleteMapping("/delete")
     public String deleteFlashcardByID(UUID id) {
         flashcardServiceImp.removeFlashcard(id);
